2bcca937bfa00b92bba42872b56895045e18218a
eeucalyptus <dev@eeucalyptus.net>
2020-12-10 23:17:32 +0100

    Working minimal executable

---
+++ src/main.c
@@ -0,0 +1,44 @@
+//#include "stm32f4xx.h"
+//#include "stm32f4xx_gpio.h"
+//
+//#include "led.h"
+//#include "Log.h"
+//#include "AudioPWM.h"
+//#include "Flash.h"
+//#include "Serial.h"
+//#include "Keypad.h"
+
+#include <unistd.h>
+
+#include <stdio.h>
+
+#include <string.h>
+
+//#include "Soundboard.h"
+
+
+int main() {
+	while(1);
+}
+//{
+//
+//
+//	Led_Init();
+//	Flash_Init();
+//
+//	// Run serial rom loader if needed
+//	//Serial_Run();
+//
+//	AudioPWM_Init();
+//	Keypad_Init();
+//
+//	for(int i = 0; i<1000000; i++);
+//
+//	// Run soundboard
+//	Soundboard_Run();
+//
+//	while(1) {
+//		asm volatile("nop");
+//	}
+//	return 0;
+//}
