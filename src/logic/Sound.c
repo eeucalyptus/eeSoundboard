cedf5c99e6be464180f21bed303e9399a432294a
eeucalyptus <dev@eeucalyptus.net>
2020-12-18 22:49:26 +0100

    Rework sound interface to work with filesystem

--- src/logic/Sound.c
+++ src/logic/Sound.c
@@ -9,84 +9,39 @@
 #include "Sound.h"
 
 #include <hw/Flash.h>
+#include <fatfs/ff.h>
 
 #include <stdbool.h>
 #include <stdint.h>
 
 uint32_t Sound_SamplesLeft;
 
+FATFS fs;
+
+FIL currentSound;
+
 void Sound_Start(int n) {
-	Flash_StopRead();
-	Flash_StartRead(0);
-	Sound_SamplesLeft = 512;
-
-	return;
-	uint8_t manufacturer;
-	uint8_t device;
-
-	Flash_ReadID(&manufacturer, &device);
-
-	int i;
-	uint8_t buf[4];
-	uint32_t length = 0;
-	uint32_t nextStartAddress = 0;
-
-	// Find the start of the sample recursively
-	for(i = 0; i <= n; i++) {
-		// Restart reading from flash at the next start address
-		Flash_StopRead();
-		for(int i = 0; i < 10000; i++);
-		Flash_StartRead(nextStartAddress);
-
-		buf[0] = Flash_Read();
-		buf[1] = Flash_Read();
-		buf[2] = Flash_Read();
-		buf[3] = Flash_Read();
-
-		// Read sound length
-		*((uint8_t *)(&length) + 0) = buf[0];
-		*((uint8_t *)(&length) + 1) = buf[1];
-		*((uint8_t *)(&length) + 2) = buf[2];
-		*((uint8_t *)(&length) + 3) = buf[3];
-
-		// calculate next start address
-		nextStartAddress += 4 + length*2;
+	// Mount if necessary
+	if(fs.fs_type == 0) {
+		f_mount(&fs, "", 0);
 	}
 
-	Sound_SamplesLeft = length;
+	f_open(&currentSound, "data.wav", FA_READ);
+
+	// TODO check RIFF structure
+
+	f_lseek(&currentSound, 44);
 }
 
 void Sound_Stop(void) {
-	Flash_StopRead();
+	f_close(&currentSound);
 }
 
-static int16_t testsample = 0;
-static int i = 0;
 bool Sound_GetSample(int16_t *sample) {
-	uint8_t data[2];
-
-	// Check if sound has ended
-	if(Sound_SamplesLeft == 0) {
-		// Sound has ended
+	UINT br;
+	f_read(&currentSound, sample, 2, &br);
+	if(br != 2) {
 		return false;
 	}
-	else {
-		// Read two bytes to buffer
-		data[0] = Flash_Read();
-		data[1] = Flash_Read();
-
-		// Write sample
-		int16_t x = testsample + 901;
-		if(testsample > x) {
-			i++;
-		}
-		testsample += 901;
-		*sample = (int16_t)(((uint16_t)data[0] << 8) | ((uint16_t)data[1]));
-//		*sample = testsample;
-
-		// Decrease numbers of samples left in sound
-		Sound_SamplesLeft--;
-
-		return true;
-	}
+	return true;
 }
--- src/main.c
+++ src/main.c
@@ -13,41 +13,22 @@
 #include <unistd.h>
 #include <stdint.h>
 
-FATFS fs;
-
 int main()
 {
 	Clock_Init();
+	Led_Init();
 	Flash_Init();
-	f_mount(&fs, "", 0);
-	FIL f;
-	f_open(&f, "data.wav", FA_READ);
 
-	static uint8_t data[4096];
-	UINT br;
+	AudioPWM_Init();
+	Keypad_Init();
 
-	f_read(&f, data, 4096, &br);
+	for(int i = 0; i<1000000; i++);
 
-	while(1);
+	// Run soundboard
+	Soundboard_Run();
+
+	while(1) {
+		asm volatile("nop");
+	}
+	return 0;
 }
-//{
-//	Clock_Init();
-//	Led_Init();
-//	Flash_Init();
-//
-//	// Run serial rom loader if needed
-//	Serial_Run();
-//
-//	AudioPWM_Init();
-//	Keypad_Init();
-//
-//	for(int i = 0; i<1000000; i++);
-//
-//	// Run soundboard
-//	Soundboard_Run();
-//
-//	while(1) {
-//		asm volatile("nop");
-//	}
-//	return 0;
-//}
